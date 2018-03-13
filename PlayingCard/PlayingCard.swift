e54a6133d89406a80ec78875cbef26ccf5a868f1
Shannon <shannon@Mirkwood.local>
2018-03-13 13:13:39 -0500

    really this time

--- PlayingCard/PlayingCard.swift
+++ PlayingCard/PlayingCard.swift
@@ -7,3 +7,71 @@
 //
 
 import Foundation
+struct PlayingCard: CustomStringConvertible {
+    var description: String  {
+        return "\(rank)\(suit)"
+    }
+    
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit: String, CustomStringConvertible {
+        var description: String {return rawValue}
+        
+        case spades = "♠️"
+        case hearts = "♥️"
+        case diamonds = "♦️"
+        case clubs = "♣️"
+        
+        static var all = [Suit.spades, .hearts, .diamonds, .clubs]
+    }
+    
+    //This is bad code, but it's used to demonstrate case...where
+    enum Rank: CustomStringConvertible {
+        var description: String {
+            switch self {
+            case .ace:
+                return "A"
+            case .numeric(let pips): return String(pips)
+            case .face(let kind): return kind
+            }
+        }
+        
+        case ace
+        case numeric(Int)
+        case face(String)
+        
+        var order: Int {
+            switch self {
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind == "J": return 11
+            case .face(let kind) where kind == "Q": return 12
+            case .face(let kind) where kind == "K": return 13
+            //This is bad code
+            default: return 0
+                
+            }
+        }
+        static var all: [Rank] {
+            var allRanks = [Rank.ace]
+            for pips in 2...10 {
+                allRanks.append(Rank.numeric(pips))
+            }
+            allRanks += [Rank.face("J"), .face("Q"), .face("K")]
+            return allRanks
+        }
+    }
+}
+
+extension Int {
+    var arc4random:Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+    }
+}
